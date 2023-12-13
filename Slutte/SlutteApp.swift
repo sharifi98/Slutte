e366f4bfdba161d6ba50b71437367b11de9f3075
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-12-13 21:50:23 +0100

    Initial Commit

---
+++ Slutte/SlutteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlutteApp.swift
+//  Slutte
+//
+//  Created by Hossein Sharifi on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SlutteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
